daa29042ae6765b270a820cd282f81ce2d9f5543
TerryW2019 <50369189+TerryW2019@users.noreply.github.com>
2020-04-07 03:19:06 -0400

    Havent written docstring

--- dict.c
+++ dict.c
@@ -27,5 +27,45 @@ int findDict(struct dictNode* target){
 	return ((int)*(target->word) - 65);
 }
 
+int getSize(struct linkedDict* dict){
+	return dict->size;
+}
+
+char* getAuthor(struct linkedDict* dict){
+	return dict->author;
+}
+
+char* getDef(struct dictNode* target, struct linkedDict** dict){
+	int c = findDict(target);
+
+	if(strcmp(target->word, dict[c]->root->word) == 0){
+		return dict[c]->root->def;
+	}
+	else if(strcmp(target->word, dict[c]->root->word) < 0){
+		return getDef_Helper(target, dict[c]->root->leftChild);
+	}
+	else if(strcmp(target->word, dict[c]->root->word) > 0){
+		return getDef_Helper(target, dict[c]->root->rightChild);
+	}
+
+	printf("%s not found in dict.\n", target->word);
+	return NULL;
+}
+
+char* getDef_Helper(struct dictNode* target, struct dictNode* subroot){
+	if(strcmp(target->word, subroot->word) == 0){
+		return subroot->def;
+	}
+	else if(strcmp(target->word, subroot->word) < 0){
+		return getDef_Helper(target, subroot->leftChild);
+	}
+	else if(strcmp(target->word, subroot->word) > 0){
+		return getDef_Helper(target, subroot->rightChild);
+	}
+
+	printf("%s not found in dict.\n", target->word);
+	return NULL;
+}
+
 
 
